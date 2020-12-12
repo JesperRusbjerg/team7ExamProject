21c309690df0141917ceb6353cfb94c7f0383637
jesper <cph-jr211@cphbusiness.dk>
2020-12-12 18:28:15 +0100

    uncomment

--- esb/src/main/java/com/team7/esb/controller/LoginController.java
+++ esb/src/main/java/com/team7/esb/controller/LoginController.java
@@ -1,35 +1,35 @@
-//package com.team7.esb.controller;
-//
-//import com.team7.esb.dto.UserDTO;
-//import com.team7.esb.utils.UtilsFunctions;
-//import loginModule.LoginModule;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.security.auth.spi.LoginModule;
-//
-//
-//@RestController
-//public class LoginController {
-//
-//    private static final String IP = UtilsFunctions.getStringEnvOrDefault("LOGIN-IP", "localhost");
-//    private static final int PORT = UtilsFunctions.getIntEnvOrDefault("LOGIN-PORT", 3001);
-//
-//    @PostMapping("/login")
-//    public String login(@RequestBody UserDTO login) {
-//        LoginModule lm = new LoginModule(LoginController.IP, LoginController.PORT);
-//        String sessionId = lm.login(login.username, login.password);
-//        return sessionId;
-//    }
-//
-//    @PostMapping("/create-user")
-//    public boolean createUser(@RequestBody UserDTO user) {
-//        LoginModule lm = new LoginModule(LoginController.IP, LoginController.PORT);
-//        return lm.createUser(user.username, user.password);
-//    }
-//
-//    @GetMapping("/verify")
-//    public boolean verifySessionId(@RequestParam String session) {
-//        LoginModule lm = new LoginModule(LoginController.IP, LoginController.PORT);
-//        return lm.verifySessionId(session);
-//    }
-//}
+package com.team7.esb.controller;
+
+import com.team7.esb.dto.UserDTO;
+import com.team7.esb.utils.UtilsFunctions;
+import loginModule.LoginModule;
+import org.springframework.web.bind.annotation.*;
+
+import javax.security.auth.spi.LoginModule;
+
+
+@RestController
+public class LoginController {
+
+    private static final String IP = UtilsFunctions.getStringEnvOrDefault("LOGIN-IP", "localhost");
+    private static final int PORT = UtilsFunctions.getIntEnvOrDefault("LOGIN-PORT", 3001);
+
+    @PostMapping("/login")
+    public String login(@RequestBody UserDTO login) {
+        LoginModule lm = new LoginModule(LoginController.IP, LoginController.PORT);
+        String sessionId = lm.login(login.username, login.password);
+        return sessionId;
+    }
+
+    @PostMapping("/create-user")
+    public boolean createUser(@RequestBody UserDTO user) {
+        LoginModule lm = new LoginModule(LoginController.IP, LoginController.PORT);
+        return lm.createUser(user.username, user.password);
+    }
+
+    @GetMapping("/verify")
+    public boolean verifySessionId(@RequestParam String session) {
+        LoginModule lm = new LoginModule(LoginController.IP, LoginController.PORT);
+        return lm.verifySessionId(session);
+    }
+}
--- esb/src/main/java/com/team7/esb/controller/StatisticsController.java
+++ esb/src/main/java/com/team7/esb/controller/StatisticsController.java
@@ -1,24 +1,10 @@
 package com.team7.esb.controller;
 
-import com.team7.esb.dto.BankProxyResponseDTO;
-import com.team7.esb.dto.LogRequestDTO;
-import com.team7.esb.entity.LoginLogStats;
-import com.team7.esb.rpcInterfaces.ILogEngine;
-import org.json.JSONException;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @RestController
 public class StatisticsController {
