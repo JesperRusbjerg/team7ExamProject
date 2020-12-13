af4b067248ed5150b13c1b19a3e490c85ebe6c56
jesper <cph-jr211@cphbusiness.dk>
2020-12-13 16:18:02 +0100

    currencyComplete

---
+++ esb/src/main/java/com/soap/UsdToDkInput.java
@@ -0,0 +1,111 @@
+
+package com.soap;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="fromCurrency" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="toCurrency" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="amount" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "fromCurrency",
+    "toCurrency",
+    "amount"
+})
+@XmlRootElement(name = "UsdToDkInput")
+public class UsdToDkInput {
+
+    @XmlElement(required = true)
+    protected String fromCurrency;
+    @XmlElement(required = true)
+    protected String toCurrency;
+    protected int amount;
+
+    /**
+     * Gets the value of the fromCurrency property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFromCurrency() {
+        return fromCurrency;
+    }
+
+    /**
+     * Sets the value of the fromCurrency property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFromCurrency(String value) {
+        this.fromCurrency = value;
+    }
+
+    /**
+     * Gets the value of the toCurrency property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getToCurrency() {
+        return toCurrency;
+    }
+
+    /**
+     * Sets the value of the toCurrency property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setToCurrency(String value) {
+        this.toCurrency = value;
+    }
+
+    /**
+     * Gets the value of the amount property.
+     * 
+     */
+    public int getAmount() {
+        return amount;
+    }
+
+    /**
+     * Sets the value of the amount property.
+     * 
+     */
+    public void setAmount(int value) {
+        this.amount = value;
+    }
+
+}
